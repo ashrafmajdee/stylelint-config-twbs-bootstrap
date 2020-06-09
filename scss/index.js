43fa5df63d8192d53bd243f6b07659b81dc7fc55
XhmikosR <xhmikosr@gmail.com>
2020-06-09 16:27:30 +0300

    Enable scss/map-keys-quotes

--- scss/index.js
+++ scss/index.js
@@ -21,6 +21,7 @@ module.exports = {
     'scss/dollar-variable-colon-space-before': 'never',
     'scss/dollar-variable-no-missing-interpolation': true,
     'scss/dollar-variable-pattern': '^[a-z][a-z-\\d]*$',
+    'scss/map-keys-quotes': 'always',
     'scss/media-feature-value-dollar-variable': null,
     'scss/no-duplicate-dollar-variables': [
       null,
